7e8ef1148b45f0691fc663b10de63b089ecf7e1c
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2022-07-28 23:36:54 +0530

    Update main.py
    Adding edit_contacts() and view_contacts() functions.
    Basic Contact-Book functions are ready

--- Contact-Book/main.py
+++ Contact-Book/main.py
@@ -37,5 +37,51 @@ def del_contact():
     else: 
         print("The Contact name entered does not exit.")
 
+def edit_contact():
+    global contacts
+    while True:
+        print("\nYour Contacts: ")
+        for item in contacts:
+            print(f'> {item}')
+
+        name_edit = input("\nEnter the name of the Contact you want to edit: ")
+
+        if name_edit in contacts:
+            print("Contact Details-")
+            print(f"> {name_edit}")
+            for item in contacts[name_edit]:
+                print(f"> {contacts[name_edit][item]}")
+
+            print("\nPlease write the attribute you want to change, and the new data in the below mentioned format :-\n<attribute>|<editted_info>")
+            print("\nFollowing are the attributes you can edit:\n|--address--|\n|--phone--|\n|--email--|")
+            user_edit = input('\n').split('|')
+
+            contacts[name_edit][user_edit[0]] = user_edit[1] 
+            print("\nContact details updated successfully \N{check mark}")
+
+            if input("Do you want to update your contacts again (y/n) ?: ").lower() != 'y': break
+
+        else: 
+            print("The Contact name entered does not exit.")
+
+def view_details():
+    global contacts
+    while True:
+        print("Your Contacts-")
+        for item in contacts.keys():
+            print(f"> {item}")
+        
+        name_info = input("\nEnter the name of the Contact whose details you wish to view: ")
+
+        if name_info in contacts:
+            print(f"\nContact Details for {name_info} :-")
+            for item in contacts[name_info]:
+                print(f">> {contacts[name_info][item]}")
+                
+            if input("Do you want to update your contacts again (y/n) ?: ").lower() != 'y': break
+        else: 
+            print("\nThe Contact name entered does not exist")
+
 new_contact()
-del_contact()
+edit_contact()
+view_details()
